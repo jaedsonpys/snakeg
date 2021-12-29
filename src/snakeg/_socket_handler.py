50066803510579597d40bb810e00030c99f28f67
Jaedson Silva <contacanal798@gmail.com>
2021-12-29 09:37:07 -0300

    modify: adicionando docstrings em métodos de _socket_handler.SocketHandler.

--- src/snakeg/_socket_handler.py
+++ src/snakeg/_socket_handler.py
@@ -2,9 +2,24 @@ import socket
 
 
 class SocketHandler:
+    def __init__(self) -> None:
+        """SocketHandler permite criar novos
+        sockets e receber conexões de clientes.
+        """
+
+        self.server_s = None
+
     def create_socket_server(
         self, host: str, port: int
     ) -> None:
+        """Cria um novo socket
+        no host e porta especificados.
+
+        :param host:
+        :param port:
+        :return:
+        """
+
         address = (host, port)
 
         self.server_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,12 +28,23 @@ class SocketHandler:
         self.server_s.listen(5024)
 
     def wait_connection(self) -> [socket.SocketType, str]:
+        """Aguarda novas conexões ao
+        socket.
+
+        :return: Retorna o socket do cliente e a mensagem recebida.
+        """
+
         client_s, addr = self.server_s.accept()
         http_message = client_s.recv(5024).decode()
 
         return client_s, http_message
 
     def close_server(self) -> None:
+        """Fecha o socket.
+
+        :return:
+        """
+
         self.server_s.shutdown(0)
 
 
