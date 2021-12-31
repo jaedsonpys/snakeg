a5c004e17366e16965f65ae493c5b5a5afc690b1
Jaedson Silva <contacanal798@gmail.com>
2021-12-31 11:35:54 -0300

    add: classe Response para criar respostas ao cliente criada.

--- src/snakeg/response.py
+++ src/snakeg/response.py
@@ -3,8 +3,30 @@ from _process_request import build_http_message
 
 class Response:
     def __init__(
-        self, body,
+        self,
+        body,
+        status: int = 200,
         headers: dict = None,
         cookies: dict = None
     ) -> object:
-        pass
+        """Cria uma resposta ao cliente.
+
+        __call__ retornará a mensagem HTTP
+        em bytes e é utilizada no envio
+        da resposta pelo socket ao cliente.
+
+        :param body: Corpo da resposta
+        :param status: Status
+        :param headers: Headers da resposta
+        :param cookies: Cookies da resposta
+        """
+
+        self._response = build_http_message(body, status, headers, cookies)
+
+    def __call__(self):
+        return self._response
+
+
+if __name__ == '__main__':
+    test = Response('Hello World!')
+    print(test())
